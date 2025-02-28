b198e04f33c7b3f6907853061d91726413caabf7
Cheick Diaby <cheick.diaby@inetum.com>
2025-02-28 21:55:41 +0100

    Ajoutez des fichiers projet.

---
+++ TicketIvoire.Domain/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketIvoire.Domain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
