2aa05f6a5a7862c2193fa077532c2182a59a9e2d
Cheick Diaby <cheick.diaby@inetum.com>
2025-03-06 21:03:38 +0100

    Ajout PersisterEventHandler

---
+++ TicketIvoire.Shared.Infrastructure/Persistence/PersisterEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using TicketIvoire.Shared.Domain.Events;
+
+namespace TicketIvoire.Shared.Infrastructure.Persistence;
+
+public abstract class PersisterEventHandler<TDomainEvent> : IDomainEventHandler<TDomainEvent> where TDomainEvent : IDomainEvent
+{
+    public abstract Task HandleAsync(TDomainEvent tEvent, CancellationToken cancellationToken);
+    public bool IsTransactional() => true;
+}
