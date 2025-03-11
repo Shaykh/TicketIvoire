ef98cacf055d46a9c254346ea65eace06265a78f
Cheick Diaby <cheick.diaby@inetum.com>
2025-03-11 20:47:19 +0100

    Migration Admin Initial

---
+++ TicketIvoire.Administration.Infrastructure/Migrations/20250310183206_Initial.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+#pragma warning disable 
+namespace TicketIvoire.Administration.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Administration");
+
+            migrationBuilder.CreateTable(
+                name: "LieuEvenements",
+                schema: "Administration",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nom = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "character varying(800)", maxLength: 800, nullable: false),
+                    Adresse = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    Ville = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Capacite = table.Column<long>(type: "bigint", nullable: true),
+                    Latitude = table.Column<decimal>(type: "numeric", nullable: true),
+                    Longitude = table.Column<decimal>(type: "numeric", nullable: true),
+                    RaisonsRetrait = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    UpdatedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LieuEvenements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Membres",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Login = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Nom = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Prenom = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Telephone = table.Column<string>(type: "character varying(18)", maxLength: 18, nullable: false),
+                    DateAdhesion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EstActif = table.Column<bool>(type: "boolean", nullable: false),
+                    StatutAdhesion = table.Column<short>(type: "smallint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    UpdatedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Membres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropositionEvenements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nom = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    DateDebut = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DateFin = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PropositionStatut = table.Column<short>(type: "smallint", nullable: false),
+                    Lieu_Nom = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Lieu_Description = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Lieu_Ville = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    Lieu_LieuEvenementId = table.Column<Guid>(type: "uuid", nullable: true),
+                    UtilisateurId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Decision_UtilisateurId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Decision_DateDecision = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Decision_Code = table.Column<string>(type: "text", nullable: true),
+                    Decision_Raisons = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    UpdatedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropositionEvenements", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LieuEvenements",
+                schema: "Administration");
+
+            migrationBuilder.DropTable(
+                name: "Membres");
+
+            migrationBuilder.DropTable(
+                name: "PropositionEvenements");
+        }
+    }
+}
+#pragma warning restore
---
+++ TicketIvoire.Administration.Infrastructure/Persistence/AdministrationContextDesignTimeFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TicketIvoire.Administration.Infrastructure.Persistence;
+
+public class AdministrationContextDesignTimeFactory : IDesignTimeDbContextFactory<AdministrationDbContext>
+{
+    public AdministrationDbContext CreateDbContext(string[] args)
+    {
+        DbContextOptions<AdministrationDbContext> options = new DbContextOptionsBuilder<AdministrationDbContext>()
+            .UseNpgsql("DevConnection")
+            .Options;
+        return new AdministrationDbContext(options);
+    }
+}
